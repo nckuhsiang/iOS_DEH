dac408aeed3cfa2da48327c08d79a4e278279b59
moebear <a82096367@gmail.com>
2020-12-01 13:49:58 +0800

    Initial Commit

---
+++ DehSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DehSwiftUI
+//
+//  Created by 阮盟雄 on 2020/12/1.
+//  Copyright © 2020 mmlab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
