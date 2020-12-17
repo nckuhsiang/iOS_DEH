7b736e7c250ab482424924609c9e23c807983a63
moebear <a82096367@gmail.com>
2020-12-17 15:37:36 +0800

    Upload Network Model

---
+++ DehSwiftUI/Network/NetworkConnector.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkConnector.swift
+//  DehSwiftUI
+//
+//  Created by 阮盟雄 on 2020/12/2.
+//  Copyright © 2020 mmlab. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class NetworkConnector{
+    struct SimpleMessage: Codable{
+        var result: String;
+    };
+    let requestModel = RequestModel()
+    let reponseModel = ResponseModel()
+    
+    func Connect(_ APIUrl:String,_ parameters: Dictionary<String, AnyObject>,_ responseFunction:@escaping (AnyObject)->Void){
+        AF.request(APIUrl, method: .post, parameters: parameters)
+            .responseJSON{ response in
+//                do {
+//                    let decoder = JSONDecoder();
+//                    try decoder.decode(SimpleMessage.self, from: response.value as! Data);
+//                } catch {
+//                    fatalError("Couldn't parse")
+//                }
+                let result = response.value as AnyObject
+                responseFunction(result)
+//                if let result = response.value {
+//                    let JSON = result as! NSDictionary
+//                    print(JSON)
+//                }
+            }
+    }
+
+    func login() -> Bool{
+        return true
+    }
+}
